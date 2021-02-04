3e1e72a7ae1481cdd543b32414f5c8dd11331a36
talles-coder <talleswendrel@gmail.com>
2021-02-04 18:27:02 -0300

    make it better

--- Desktop/tutorial_api/README.md
+++ Desktop/tutorial_api/README.md
@@ -3,3 +3,6 @@ npm init
 
 //gerencia as requisições, rotas e URLs, entre outras funcionalidades 
 npm install express
+
+//instalar o módulo para reiniciar o servidor sempre que houver alteração no código fonte
+npm install -D nodemon
--- Desktop/tutorial_api/app.js
+++ Desktop/tutorial_api/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("introdução a API")
+    return res.json({titulo: "Como criar API??????"})
 });
 
 app.listen(8080, () => {
